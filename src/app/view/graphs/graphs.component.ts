6d0a8a1f52269a9ea28ffa4588250f26d1b53a9c
Selva <selvaraj.arumuthu@gmail.com>
2017-10-29 21:35:34 -0400

    graph update

--- src/app/view/graphs/graphs.component.ts
+++ src/app/view/graphs/graphs.component.ts
@@ -13,77 +13,97 @@ export class GraphsComponent implements OnInit {
 
   showGraph: boolean;
   showLoader: boolean;
-  // lineData: any[];
+  lineData: any[];
   graphData = {};
   subscription: Subscription;
   gsm_data: GsmData[];
   bf_data: BfData[];
   title: string;
+
   constructor(private serverService: ServerService) {
     this.showGraph = false;
   }
-  
+
   ngOnInit() {
   }
-  
-  viewGSMGraph() {
+
+  showGSMYearly() {
     this.showLoader = true;
-    // alert("GSM Graph");
-    var lineData = [];
+    this.lineData = [];
     this.subscription = this.serverService.getTallyGSMData().
-    subscribe(list => {
-      this.gsm_data = list;
-      // console.log(this.gsm_data);
-      // this.convertArray();
-      // var lineData: any[];
-      lineData[0] = ['Type', "Target", "Actual"];
-      for (var i = 0; i < this.gsm_data.length; i++) {
-        var type = this.gsm_data[i].voucherEffectiveDate + this.gsm_data[i].stockItemName;
-        var target = this.gsm_data[i].gsmAct;
-        var actual = this.gsm_data[i].gsmTgt;
-        lineData[i + 1] = [type, target, actual];
+      subscribe(list => {
+        this.gsm_data = list;
+        this.lineData[0] = ['Type', "Target", { 'type': 'string', 'role': 'tooltip', 'p': { 'html': false } }, "Actual", { 'type': 'string', 'role': 'tooltip', 'p': { 'html': true } }];
+        for (var i = 0; i < this.gsm_data.length; i++) {
+          var type = this.gsm_data[i].voucherEffectiveDate;
+          var actual = this.gsm_data[i].gsmTgt;
+          var target_tooltip = "Date: " + this.gsm_data[i].voucherEffectiveDate + ", Item: " + this.gsm_data[i].stockItemName + ", Target:" + this.gsm_data[i].gsmTgt + " vs Actual:" + this.gsm_data[i].gsmAct;
+          var target = this.gsm_data[i].gsmAct;
+          var actual_tooltip = "Date: " + this.gsm_data[i].voucherEffectiveDate + ", Item: " + this.gsm_data[i].stockItemName + ", Target:" + this.gsm_data[i].gsmTgt + " vs Actual:" + this.gsm_data[i].gsmAct;
+          this.lineData[i + 1] = [type, target, target_tooltip, actual, actual_tooltip];
+        }
+        this.showLoader = false;
+        this.title = "GSM variance yearly trend";
+        this.showGraph = true;
+        this.displayGraph(this.lineData);
       }
-      this.showGraph = true;
-      this.showLoader = false;
-      this.title = "GSM variance yearly trend";
-      this.displayGraph(lineData);
-    }
-  )
-  // var lineData = [
-    //   ["type", "Target", "Actual"],
-    //   ["2017-10-02120 GSM 100.5CM", 120.00, 120.00],
-    //   ["2017-10-02120 GSM 91.5CM", 117.00, 112.00]]
-    // this.showGraph = true;
-    // this.displayGraph(lineData);
-    // alert(lineData);
-    // lineData = [];
-    // this.showGraph = false;
+      )
   }
-  
+
+  monthlyGSMGraph() {
+    // this.subscription = this.serverService.getMonthlyGSMData().
+  }
+  weeklyGSMGraph() {
+    // this.subscription = this.serverService.getWeeklyGSMData().
+  }
+
+  showBFYearly() {
+    this.showLoader = true;
+    this.lineData = [];
+    this.subscription = this.serverService.getTallyBFData().
+      subscribe(list => {
+        this.bf_data = list;
+        this.lineData[0] = ['Type', "Target", { 'type': 'string', 'role': 'tooltip', 'p': { 'html': false } }, "Actual", { 'type': 'string', 'role': 'tooltip', 'p': { 'html': true } }];
+        for (var i = 0; i < this.bf_data.length; i++) {
+          var type = this.bf_data[i].voucherEffectiveDate;
+          var actual = this.bf_data[i].bfTgt;
+          var target_tooltip = "Date: " + this.bf_data[i].voucherEffectiveDate + ", Item: " + this.bf_data[i].stockItemName + ", Target:" + this.bf_data[i].bfTgt + " vs Actual:" + this.bf_data[i].bfAct;
+          var target = this.bf_data[i].bfAct;
+          var actual_tooltip = "Date: " + this.bf_data[i].voucherEffectiveDate + ", Item: " + this.bf_data[i].stockItemName + ", Target:" + this.bf_data[i].bfTgt + " vs Actual:" + this.bf_data[i].bfAct;
+          this.lineData[i + 1] = [type, target, target_tooltip, actual, actual_tooltip];
+        }
+        this.showLoader = false;
+        this.title = "BF variance yearly trend";
+        this.showGraph = true;
+        this.displayGraph(this.lineData);
+      }
+      )
+  }
+
   viewBFGraph() {
     // alert("BF Graph");
     this.showLoader = true;
     var lineData = [];
     this.subscription = this.serverService.getTallyBFData().
-    subscribe(list => {
-      this.bf_data = list;
-      // console.log(this.gsm_data);
-      // this.convertArray();
-      // var lineData: any[];
-      lineData[0] = ['Type', "Target", "Actual"];
-      for (var i = 0; i < this.bf_data.length; i++) {
-        var type = this.bf_data[i].voucherEffectiveDate + this.bf_data[i].stockItemName;
-        var type = this.bf_data[i].voucherEffectiveDate + " " + this.bf_data[i].stockItemName;
-        var target = this.bf_data[i].bfAct;
-        var actual = this.bf_data[i].bfTgt;
-        lineData[i + 1] = [type, target, actual];
+      subscribe(list => {
+        this.bf_data = list;
+        // console.log(this.gsm_data);
+        // this.convertArray();
+        // var lineData: any[];
+        lineData[0] = ['Type', "Target", "Actual"];
+        for (var i = 0; i < this.bf_data.length; i++) {
+          var type = this.bf_data[i].voucherEffectiveDate + this.bf_data[i].stockItemName;
+          var type = this.bf_data[i].voucherEffectiveDate + " " + this.bf_data[i].stockItemName;
+          var target = this.bf_data[i].bfAct;
+          var actual = this.bf_data[i].bfTgt;
+          lineData[i + 1] = [type, target, actual];
+        }
+        this.showGraph = true;
+        this.showLoader = false;
+        this.title = "BF variance yearly trend";
+        this.displayGraph(lineData);
       }
-      this.showGraph = true;
-      this.showLoader = false;
-      this.title = "BF variance yearly trend";
-      this.displayGraph(lineData);
-    }
-    )
+      )
     // var lineData = [
     //   ["type", "Target", "Actual"],
     //   ["2017-10-02120 GSM 100.5CM", 120.00, 120.00],
@@ -94,6 +114,57 @@ export class GraphsComponent implements OnInit {
     // this.showGraph = false;
   }
 
+  weeklyBFGraph() {
+    // alert("BF Graph");
+    this.showLoader = true;
+    var lineData = [];
+    this.subscription = this.serverService.getWeeklyBFData().
+      subscribe(list => {
+        this.bf_data = list;
+        // console.log(this.gsm_data);
+        // this.convertArray();
+        // var lineData: any[];
+        lineData[0] = ['Type', "Target", "Actual"];
+        for (var i = 0; i < this.bf_data.length; i++) {
+          var type = this.bf_data[i].voucherEffectiveDate + this.bf_data[i].stockItemName;
+          var type = this.bf_data[i].voucherEffectiveDate + " " + this.bf_data[i].stockItemName;
+          var target = this.bf_data[i].bfAct;
+          var actual = this.bf_data[i].bfTgt;
+          lineData[i + 1] = [type, target, actual];
+        }
+        this.showGraph = true;
+        this.showLoader = false;
+        this.title = "BF variance weekly";
+        this.displayGraph(lineData);
+      }
+      )
+  }
+
+  monthlyBFGraph() {
+    // alert("BF Graph");
+    this.showLoader = true;
+    var lineData = [];
+    this.subscription = this.serverService.getMonthlyBFData().
+      subscribe(list => {
+        this.bf_data = list;
+        // console.log(this.gsm_data);
+        // this.convertArray();
+        // var lineData: any[];
+        lineData[0] = ['Type', "Target", "Actual"];
+        for (var i = 0; i < this.bf_data.length; i++) {
+          var type = this.bf_data[i].voucherEffectiveDate + this.bf_data[i].stockItemName;
+          var type = this.bf_data[i].voucherEffectiveDate + " " + this.bf_data[i].stockItemName;
+          var target = this.bf_data[i].bfAct;
+          var actual = this.bf_data[i].bfTgt;
+          lineData[i + 1] = [type, target, actual];
+        }
+        this.showGraph = true;
+        this.showLoader = false;
+        this.title = "BF variance monthly trend";
+        this.displayGraph(lineData);
+      }
+      )
+  }
   displayGraph(lineData) {
     this.graphData = {
       chartType: 'LineChart',
@@ -104,7 +175,14 @@ export class GraphsComponent implements OnInit {
         tooltip: { isHtml: true },
         width: 1080,
         height: 500,
-        colors: ['green', 'orange']
+        colors: ['green', 'orange'],
+        hAxis: {
+          title: 'Date'
+          // ticks: [100,200]
+        },
+        vAxis: {
+          title: 'GSM'
+        }
       }
     };
   }
